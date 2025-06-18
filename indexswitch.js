bd2771ae274ba1227441fbe23faf164b31edc891
“Erick” <152939401+bratugues@users.noreply.github.com>
2025-06-18 20:40:12 +0200

    finish new solution using switch case

--- indexswitch.js
+++ indexswitch.js
@@ -15,3 +15,52 @@
 
 // Ao final deve se exibir uma mensagem:
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+let nomeHeroi = "Bratu"
+let xpHeroi = 3934
+let nivel
+
+switch (true){
+    case (xpHeroi < 1000): {
+        nivel = "Ferro"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 1001 && xpHeroi <= 2000): {
+        nivel = "Bronze"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 2001 && xpHeroi <= 5000):{
+        nivel = "Prata"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 5001 && xpHeroi <= 7000):{
+        nivel = "Ouro"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 7001 && xpHeroi <= 8000):{
+        nivel = "Platina"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 8001 && xpHeroi <= 9000):{
+        nivel = "Ascendente"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 9001 && xpHeroi <= 10000): {
+        nivel = "Imortal"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    case (xpHeroi >= 10001):{
+        nivel = "Imortal"
+        console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+        break
+    }
+    default:
+        console.log (`Não foi possível identificar o nível do herói de nome ${nomeHeroi}`)
+}
